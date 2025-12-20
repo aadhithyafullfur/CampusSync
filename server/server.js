5dbae437a185422e4ab80dca0b178b0587c183b1
Aadhithya_RathaKrishnan <aadhithyaa120@gmail.com>
2025-12-20 09:10:43 +0530

    Connected to the mongodb atlas

--- server/server.js
+++ server/server.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use("/api", require("./routes/testroutes"));
+
 app.get("/", (req, res) => {
-  res.send("Backend server is running 🚀");
+  res.send("CampusSync Backend Running ✅");
 });
 
-app.get("/api/test", (req, res) => {
-  res.json({ message: "API working successfully" });
-});
+const PORT = process.env.PORT || 5000;
 
-const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
